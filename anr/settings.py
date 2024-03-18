e77ab785947aafefc76340d6cb23a71abc3ce173
p02e909 <dangtrangnhattao@gmail.com>
2024-03-18 23:02:16 +0700

    update seckey

--- anr/settings.py
+++ anr/settings.py
@@ -22,7 +22,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = "django-insecure-egpfu4v&!69ys_7+^6yi$pg3x%7==@y#o3+#8#(dq3si3yn3=e"
+SECRET_KEY = os.environ.get("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
