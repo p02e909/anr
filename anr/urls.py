6d95f7b7e1f1b448d17ac72233e3adb90a2eb7c2
p02e909 <dangtrangnhattao@gmail.com>
2024-03-18 22:47:22 +0700

    add error when user upload not a csv file , update urls include

--- anr/urls.py
+++ anr/urls.py
@@ -1,9 +1,7 @@
-from django.urls import path
-from inbound import views
+from django.urls import path, include
 from django.contrib import admin
 
 urlpatterns = [
     path("admin/", admin.site.urls),
-    path("", views.upload_csv, name="upload_csv"),
-    path("data/", views.show_data, name="show_data"),
+    path("", include("inbound.urls")),
 ]
--- inbound/forms.py
+++ inbound/forms.py
@@ -2,4 +2,10 @@ from django import forms
 
 
 class CSVUploadForm(forms.Form):
-    file = forms.FileField()
+    file = forms.FileField(label="Select a CSV file")
+
+    def clean_file(self):
+        file = self.cleaned_data.get("file")
+        if not file.name.endswith(".csv"):
+            raise forms.ValidationError("Please upload a CSV file")
+        return file
